657af4496d9d1fb032afe5ad509bb9bb8871543b
Saurabh Singh <saurabh.rgt@gmail.com>
2023-09-11 19:09:25 +0530

    todoserver file

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -37,10 +37,10 @@
 
     - For any other route not defined in the server return 404
 
-  Testing the server - run `npm run test-todoServer` command in terminal
+
  */
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
